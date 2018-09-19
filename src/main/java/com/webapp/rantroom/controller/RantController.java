40edc53af0a952270f037c183e7f2b42286a6168
Saad Khan <khan.ssaad@gmail.com>
2018-09-19 17:52:53 +0530

    updated java files

--- src/main/java/com/webapp/rantroom/controller/RantController.java
+++ src/main/java/com/webapp/rantroom/controller/RantController.java
@@ -2,11 +2,20 @@ package com.webapp.rantroom.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.webapp.rantroom.exception.ResourceNotFoundException;
 import com.webapp.rantroom.model.Rants;
 import com.webapp.rantroom.repository.RantRepository;
 
@@ -24,10 +33,39 @@ public class RantController {
 	}
 
     // Create a new Rant
+	@PostMapping("/rants")
+	public Rants createNote(@Valid @RequestBody Rants rant) {
+	    return rantRepository.save(rant);
+	}
 
     // Get a Single Rant
-
+	@GetMapping("/rants/{id}")
+	public Rants getNoteById(@PathVariable(value = "id") Long rantId) {
+	    return rantRepository.findById(rantId)
+	            .orElseThrow(() -> new ResourceNotFoundException("Rant", "id", rantId));
+	}
     // Update a Rant
+	@PutMapping("/rants/{id}")
+	public Rants updateNote(@PathVariable(value = "id") Long rantId,
+	                                        @Valid @RequestBody Rants rantDetails) {
 
+		Rants rant = rantRepository.findById(rantId)
+	            .orElseThrow(() -> new ResourceNotFoundException("Rant", "id", rantId));
+
+		rant.setTitle(rantDetails.getTitle());
+		rant.setContent(rantDetails.getContent());
+
+		Rants updatedRant = rantRepository.save(rant);
+	    return updatedRant;
+	}
     // Delete a Rant
+	@DeleteMapping("/notes/{id}")
+	public ResponseEntity<?> deleteNote(@PathVariable(value = "id") Long rantId) {
+		Rants rant = rantRepository.findById(rantId)
+	            .orElseThrow(() -> new ResourceNotFoundException("Rant", "id", rantId));
+
+	    rantRepository.delete(rant);
+
+	    return ResponseEntity.ok().build();
+	}
 }
